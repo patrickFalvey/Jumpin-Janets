e00fbdf98d86f123e4bde8697baebe5b7974792a
patrickFalvey <patrickfalvey40@gmail.com>
2015-03-08 11:59:29 -0600

    Revert "Minor adjustments"
    This reverts commit a18501db6ebbeff2809f96942d0471184444a996.

--- JJsoup.py
+++ JJsoup.py
@@ -2,34 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
-
-scores = []
-newScores = []
-teams = []
-clean = {}
 url = "http://scores.espn.go.com/ncb/scoreboard"
+
 r = requests.get(url)
 soup = BeautifulSoup(r.content)
 team = soup.find_all('p',{'class':'team-name'})
 score = soup.find_all('li',{'class':'final'})
-
-
+scores = []
+newScores = []
+teams = []
 for name in team:
     teams.append(name.text)
-    
 for num in score:
     scores.append(num.text)
-    
+print(scores)
 for i in scores:
     if i == 'T':
         scores.remove(i)
-        
+clean = {}
 for i in range(len(team)):
     clean[teams[i]] = scores[i]
-
-for key,value in clean.items():
-    print(str(key) + ' scored ' + str(value) + ' points.') 
-
+print(clean)
 file = open('scoreData.txt','w')
 file = file.write(str(clean))
     

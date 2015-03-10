9212d8ea335a7569ac149ea6fd3170ffb0e17d8c
patrickFalvey <patrickfalvey40@gmail.com>
2015-03-09 23:33:50 -0600

    Converted to Python 2.7
    Data is now presented in xml format.

--- JJsoup.py
+++ JJsoup.py
@@ -1,18 +1,19 @@
 import requests
-from bs4 import *
+from bs4 import BeautifulSoup
+import dicttoxml
 import sys
 import re
 
 
-scores = []
-newScores = []
-teams = []
-clean = {}
-url = "http://scores.espn.go.com/ncb/scoreboard"
-r = requests.get(url)
-soup = BeautifulSoup(r.content)
-team = soup.select('p span a')
-score = soup.find_all('li',{'class':'final'})
+scores=[]
+newScores=[]
+teams=[]
+clean={}
+url="http://scores.espn.go.com/ncb/scoreboard"
+r=requests.get(url)
+soup=BeautifulSoup(r.content)
+team=soup.select('p span a')
+score=soup.find_all('li',{'class':'final'})
 
 
 for name in team:
@@ -22,18 +23,18 @@ for num in score:
     scores.append(num.text)
     
 for i in scores:
-    if i == 'T':
+    if i=='T':
         scores.remove(i)
         
 for i in range(len(team)):
-    clean[teams[i]] = scores[i]
-
-for key,value in clean.items():
-    print(str(key) + '/ ' + str(value)) 
-
-file = open('scoreData.txt','w')
-file = file.write(str(clean))
+    clean[teams[i]]=scores[i]
     
+sweetness=dicttoxml.dicttoxml(clean)
+
+file=open('scoreData.txt','w')
+file=file.write(str(sweetness))
+    
+
 
 
 

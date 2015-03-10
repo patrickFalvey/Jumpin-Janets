96aebfb134d81f358c9d36c6d55e415d6bde96e7
patrickFalvey <patrickfalvey40@gmail.com>
2015-03-10 10:01:00 -0600

    Added: auto-run, timestamp
    Auto-run=every 5 min

--- JJsoup.py
+++ JJsoup.py
@@ -1,6 +1,8 @@
-import requests
 from bs4 import BeautifulSoup
 import dicttoxml
+import requests
+import datetime
+import time
 import sys
 
 
@@ -11,39 +13,41 @@ clean={}
 url="http://scores.espn.go.com/ncb/scoreboard"
 
 
-
-# Scrape website with Beautiful Soup
-def scrape(website):
-    r=requests.get(website)
-    soup=BeautifulSoup(r.content)
-    team=soup.select('p span a')
-    score=soup.find_all('li',{'class':'final'})
-    return team,score
-
-
-# Refine data and return it in XML format
-def dataScrub(x,y):
-    for name in x:
-        teams.append(name.text)    
-    for num in y:
-        scores.append(num.text)    
-    for i in scores:
-        if i=='T':
-            scores.remove(i)
-    for i in range(len(team)):
-        clean[teams[i]]=scores[i]
-    cleanxml=dicttoxml.dicttoxml(clean)
-    return cleanxml
+while True:
+    # Scrape website with Beautiful Soup
+    def scrape(website):
+        r=requests.get(website)
+        soup=BeautifulSoup(r.content)
+        team=soup.select('p span a')
+        score=soup.find_all('li',{'class':'final'})
+        return team,score
 
 
-# Save data to disk
-def saveData(cleanxml):   
-    scoreData=open('scoreData.txt','w')
-    scoreData=scoreData.write(str(cleanxml))
-    
+    # Refine data and return it in XML format
+    def dataScrub(x,y):
+        for name in x:
+            teams.append(name.text)    
+        for num in y:
+            scores.append(num.text)    
+        for i in scores:
+            if i=='T':
+                scores.remove(i)
+        for i in range(len(team)):
+            clean[teams[i]]=scores[i]
+        cleanxml=dicttoxml.dicttoxml(clean)
+        return cleanxml
 
-team,score=scrape(url)    
-saveData(dataScrub(team,score))
+
+    # Save data to disk
+    def saveData(cleanxml):   
+        scoreData=open('scoreData.txt','w')
+        timeStamp=scoreData.write(str(datetime.datetime.now()))
+        scoreData=scoreData.write(str(cleanxml))
+
+        
+    team,score=scrape(url)    
+    saveData(dataScrub(team,score))
+    time.sleep(300)
 
 
 

5d1a930ad231df1a064f14b6bc748fb1d771cc55
patrickFalvey <patrickfalvey40@gmail.com>
2015-03-10 08:15:37 -0600

    Refined code
    -Rewrote program in O.O.P. style
    -Added comments

--- JJsoup.py
+++ JJsoup.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import dicttoxml
 import sys
-import re
 
 
 scores=[]
@@ -10,29 +9,44 @@ newScores=[]
 teams=[]
 clean={}
 url="http://scores.espn.go.com/ncb/scoreboard"
-r=requests.get(url)
-soup=BeautifulSoup(r.content)
-team=soup.select('p span a')
-score=soup.find_all('li',{'class':'final'})
 
 
-for name in team:
-    teams.append(name.text)
-    
-for num in score:
-    scores.append(num.text)
-    
-for i in scores:
-    if i=='T':
-        scores.remove(i)
-        
-for i in range(len(team)):
-    clean[teams[i]]=scores[i]
-    
-sweetness=dicttoxml.dicttoxml(clean)
 
-file=open('scoreData.txt','w')
-file=file.write(str(sweetness))
+# Scrape website with Beautiful Soup
+def scrape(website):
+    r=requests.get(website)
+    soup=BeautifulSoup(r.content)
+    team=soup.select('p span a')
+    score=soup.find_all('li',{'class':'final'})
+    return team,score
+
+
+# Refine data and return it in XML format
+def dataScrub(x,y):
+    for name in x:
+        teams.append(name.text)    
+    for num in y:
+        scores.append(num.text)    
+    for i in scores:
+        if i=='T':
+            scores.remove(i)
+    for i in range(len(team)):
+        clean[teams[i]]=scores[i]
+    cleanxml=dicttoxml.dicttoxml(clean)
+    return cleanxml
+
+
+# Save data to disk
+def saveData(cleanxml):   
+    scoreData=open('scoreData.txt','w')
+    scoreData=scoreData.write(str(cleanxml))
+    
+
+team,score=scrape(url)    
+saveData(dataScrub(team,score))
+
+
+
     
 
 
